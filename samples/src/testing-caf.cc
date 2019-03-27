47f2e916213b32965e9d2c1d09160b4acf3a8ee1
Kapil Sachdeva <ksachdeva17@gmail.com>
2019-03-27 14:03:10 -0500

    update the testing-caf example to use the expect macro

--- samples/src/testing-caf.cc
+++ samples/src/testing-caf.cc
@@ -3,35 +3,19 @@
 #include <caf/test/dsl.hpp>
 #include <caf/test/unit_test_impl.hpp>
 
-#define ERROR_HANDLER [&](caf::error &err) { CAF_FAIL(sys.render(err)); }
-
-struct config : caf::actor_system_config {
-  config() { set("scheduler.policy", caf::atom("testing")); }
-};
-
 caf::behavior actor_under_test(caf::event_based_actor *self) {
-  return {[=](int x, int y) {
-    caf::aout(self) << "entered in behavior" << std::endl;
-    return x + y;
-  }};
+  return {[=](int x, int y) { return x + y; }};
 }
 
-CAF_TEST_FIXTURE_SCOPE(tracing_scope, test_coordinator_fixture<config>)
+struct fixture : test_coordinator_fixture<> {};
 
-CAF_TEST(divide) {
+CAF_TEST_FIXTURE_SCOPE(tracing_scope, fixture)
+
+CAF_TEST(addition) {
   auto an_actor = self->spawn(actor_under_test);
-
-  sched.run();
-  sched.inline_next_enqueue();
-  self->request(an_actor, std::chrono::seconds(10), 2, 3)
-      .receive(
-          [&](int result) {
-            caf::aout(self) << "Result is " << result << std::endl;
-          },
-          ERROR_HANDLER);
-
-  caf::aout(self) << "prints only after actor has processed request"
-                  << std::endl;
+  self->send(an_actor, 2, 3);
+  expect((int, int), from(self).to(an_actor).with(2, 3));
+  expect((int), from(an_actor).to(self).with(5));
 }
 
 CAF_TEST_FIXTURE_SCOPE_END()
